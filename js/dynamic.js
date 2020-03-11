7b136a1c2881eca0e6bd74dc758ce48427634427
abraao <abraao.lincol@gmail.com>
2020-03-11 19:30:03 -0300

    todo list

---
+++ js/dynamic.js
@@ -0,0 +1,8 @@
+function showAdd(){
+    document.querySelector('.divAddTodo').style.display = "flex"
+    document.querySelector('.divAddTodo').style.transform = "scale(1)"
+}
+
+function addTarefa(){
+    document.querySelector('.divAddTodo').style.display = "none"
+}
