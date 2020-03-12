bf806b18b8324ba536e5700031f3d7fd3a0b7a74
Abraao Lincol <abraao.lincol@gmail.com>
2020-03-12 16:29:42 -0300

    new functions add

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,8 +1,32 @@
+var list = []
+
+function mountList(){
+    document.getElementById("listOfTodos").innerHTML = ""
+
+    if(list.length > 0){
+        list.forEach( tarefa => {
+            let li = document.createElement("LI")
+            li.innerHTML = tarefa.tarefa
+            if(tarefa.completada){
+                li.style.textDecoration = "line-through"
+            }
+            document.getElementById("listOfTodos").appendChild(li)
+        })
+    }
+}
+
 function showAdd(){
     document.querySelector('.divAddTodo').style.display = "flex"
     document.querySelector('.divAddTodo').style.transform = "scale(1)"
 }
 
-function addTarefa(){
+function addTarefa(event){
+    list.push({
+        tarefa: document.getElementById("novaTarefa").value,
+        completada: false
+    })
     document.querySelector('.divAddTodo').style.display = "none"
+    document.getElementById("novaTarefa").value = ""
+    mountList()
+    event.preventDefault()
 }
