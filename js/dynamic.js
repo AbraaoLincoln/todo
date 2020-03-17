88de9e7b5141fdbb42d99dd27ed0161be32e0e8e
Abraao Lincol <abraao.lincol@gmail.com>
2020-03-16 22:11:27 -0300

    finished

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,11 +1,22 @@
 var list = []
+var selectedTask = ""
+
+function selectTask(event){
+    selectedTask = event.target.id
+    if(event.target.style.textDecoration == "underline"){
+        event.target.style.textDecoration = "none"
+    }else{
+        event.target.style.textDecoration = "underline"
+    }
+}
 
 function mountList(){
     document.getElementById("listOfTodos").innerHTML = ""
-
     if(list.length > 0){
         list.forEach( tarefa => {
             let li = document.createElement("LI")
+            li.addEventListener("click", selectTask)
+            li.id = tarefa.id
             li.innerHTML = tarefa.tarefa
             if(tarefa.completada){
                 li.style.textDecoration = "line-through"
@@ -23,10 +34,32 @@ function showAdd(){
 function addTarefa(event){
     list.push({
         tarefa: document.getElementById("novaTarefa").value,
-        completada: false
+        completada: false,
+        id: list.length
     })
     document.querySelector('.divAddTodo').style.display = "none"
     document.getElementById("novaTarefa").value = ""
     mountList()
     event.preventDefault()
+}
+
+function removeTask(){
+
+    list = list.filter((task) => {
+        if(task.id != selectedTask){
+            if(task.id > selectedTask){
+                task.id--
+            }
+            return true
+        }else{
+            return false
+        }
+    })
+    
+    mountList()
+}
+
+function checkAndUncheck(){
+    list[selectedTask].completada = !list[selectedTask].completada
+    mountList()
 }
